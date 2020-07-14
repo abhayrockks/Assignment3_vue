8826de4801b1954969ffed1b00643f1d4cf3d8b4
Abhay Mehta <abhayrockks@gmail.com>
2020-07-14 19:43:58 +0530

    second commit

---
+++ src/components/Timer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="timer">
+    <p v-if="tflag ? 'toggleTimer':'reset'">{{time}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: 0,
+      interval: null
+    };
+  },
+  props: {
+    tflag: Boolean
+  },
+
+  methods: {
+    toggleTimer() {
+      this.interval = setInterval(this.incrementTime, 1000);
+    },
+    incrementTime() {
+      this.time = parseInt(this.time) + 1;
+    },
+    reset() {
+      clearInterval(this.interval);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
